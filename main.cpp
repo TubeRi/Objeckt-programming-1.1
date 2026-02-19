927c03bac14e2ac5715c9081b3c46b4a18958536
Julius <tuberlife3000@gmail.com>
2026-02-19 02:17:14 +0200

    0.1
    su vecotriais

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iomanip>
 #include <algorithm>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -26,14 +27,25 @@ struct Student
 double Mediana(vector<int> paz)
 {
     if (paz.empty()) return 0.0;
-
     std::sort(paz.begin(), paz.end());
     int n = (int)paz.size();
-
     if (n % 2 == 0)
         return (paz[n/2 - 1] + paz[n/2]) / 2.0;
     else
         return paz[n/2];
+}
+//vid funkcija, jog nereiktu sum ir n laikyti atskirai
+double Vidurkis(const vector<int>& paz)
+{
+    if (paz.empty()) return 0.0;
+    int sum = 0;
+    for (int x : paz) sum += x;
+    return static_cast<double>(sum) / paz.size();
+}
+int RandomPazymys(std::mt19937& gen)
+{
+    static std::uniform_int_distribution<int> dist(1, 10);
+    return dist(gen);
 }
 
 void Spausdinimas(const vector<Student> &grupe, char pasirinkimas);
@@ -47,37 +59,68 @@ int main()
     cout << "Skaiciuoti galutini pagal vidurki ar mediana? (v/m): ";
     cin >> pasirinkimas;
 
-    for(int ii=0; ii<2; ii++)
-    { 
-    
-    cout<<"Iveskite varda ir pavarde: ";
-    cin>>A.vardas>>A.pavarde;
-    //std::cout << A.vardas << " " << A.pavarde << std::endl;
-    cout<<"Iveskite semestro pazymius:\nKiek pazymiu bus?";
-   int n, temp, sum = 0;
-        cin >> n;
+    char rezimas;
+    cout << "Ivedimas ranka ar atsitiktinis? (r/a): ";
+    cin >> rezimas;
 
-        for (int i = 0; i < n; i++)
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    while (true)
+    {
+        A.paz.clear();
+
+        cout << "\nIveskite pavarde (0 - baigti studentu ivedima): ";
+        cin >> A.pavarde;
+        if (A.pavarde == "0") break;
+
+        cout << "Iveskite varda: ";
+        cin >> A.vardas;
+
+        if (rezimas == 'a' || rezimas == 'A')
         {
-            cout << "Iveskite " << i + 1 << " pazymi: ";
-            cin >> temp;
-            A.paz.push_back(temp);
-            sum += temp;
-        }
+            // random num generator
+            int nd_kiek;
+            cout << "Kiek ND generuoti? ";
+            cin >> nd_kiek;
 
-        cout << "Iveskite egzamino pazymi: ";
-        cin >> A.egz;
-        double vid = (n > 0) ? (static_cast<double>(sum) / n) : 0.0;
+            for (int i = 0; i < nd_kiek; i++)
+                A.paz.push_back(RandomPazymys(gen));
+
+            A.egz = RandomPazymys(gen);
+
+            // parodo sugeneruotus pazymius, bet manrods nebutina
+            cout << "Sugeneruoti ND: ";
+            for (int x : A.paz) cout << x << " ";
+            cout << "\nSugeneruotas egz: " << A.egz << endl;
+        }
+        else
+        {
+            cout << "Iveskite ND pazymius (0 - baigti ND ivedima):\n";
+            while (true)
+            {
+                int temp;
+                cout << "ND: ";
+                cin >> temp;
+                if (temp == 0) break;
+                A.paz.push_back(temp);
+            }
+
+            cout << "Iveskite egzamino pazymi: ";
+            cin >> A.egz;
+        }
+        double vid = Vidurkis(A.paz);         
         double med = Mediana(A.paz);
+
         if (pasirinkimas == 'v' || pasirinkimas == 'V')
             A.rez = 0.4 * vid + 0.6 * A.egz;
         else
             A.rez = 0.4 * med + 0.6 * A.egz;
-        grupe.push_back(A);
 
-        A.paz.clear(); 
+        grupe.push_back(A);
     }
     Spausdinimas(grupe, pasirinkimas);
+    return 0;
 }
 void Spausdinimas(const vector<Student> &grupe, char pasirinkimas)
 {
@@ -90,6 +133,9 @@ void Spausdinimas(const vector<Student> &grupe, char pasirinkimas)
         cout << right << setw(20) << "Galutinis (Med.)" << endl;
 
     cout <<string(50, '-') << endl;
+
+    cout<<std::fixed<<std::setprecision(2);
+
     for(auto A:grupe)
     {
         std::cout<<left<<setw(15)<<A.pavarde<<left<<setw(15)<<A.vardas<<right<<setw(20)<<A.rez<<endl;
